d57e3911acdf6eb9bd743c890d0575ebe4ad8a9c
unknown <diego.andrade-d@labinfo.is.escuelaing.edu.co>
2026-03-05 13:37:16 -0500

    estructura inicial

---
+++ src/main/java/Zeus_Codensa/Techcup_Futbol/TechcupFutbolApplication.java
@@ -0,0 +1,13 @@
+package Zeus_Codensa.Techcup_Futbol;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechcupFutbolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TechcupFutbolApplication.class, args);
+	}
+
+}
---
+++ src/test/java/Zeus_Codensa/Techcup_Futbol/TechcupFutbolApplicationTests.java
@@ -0,0 +1,13 @@
+package Zeus_Codensa.Techcup_Futbol;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TechcupFutbolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
