c5122de3cc0a20e5ab6004e02d796102ae55557a
Exael74 <stiven.pardo.g74@gmail.com>
2026-04-17 10:21:53 -0500

    feat: add TeamRepository with entity graph support for player relations

--- src/main/java/com/zeuscodensa/techcupfutbol/persistence/repository/TeamRepository.java
+++ src/main/java/com/zeuscodensa/techcupfutbol/persistence/repository/TeamRepository.java
@@ -1,12 +1,19 @@
 package com.zeuscodensa.techcupfutbol.persistence.repository;
 
 import java.util.Optional;
+import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.EntityGraph;
 
 import com.zeuscodensa.techcupfutbol.persistence.entity.TeamEntity;
 
 public interface TeamRepository extends JpaRepository<TeamEntity, Long> {
+    
+    @EntityGraph(attributePaths = "players")
+    List<TeamEntity> findAll();
+
+    @EntityGraph(attributePaths = "players")
     Optional<TeamEntity> findByTeamName(String teamName);
     boolean existsByPlayersEmail(String email);
 }
